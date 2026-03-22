c20e8066d083ba9e8c1a7382c5fe22245e305811
Joel Jo <108486336+thagreatjoel@users.noreply.github.com>
2026-03-23 00:52:06 +0530

    Update oauth.js

--- netlify/functions/oauth.js
+++ netlify/functions/oauth.js
@@ -2,7 +2,7 @@ const { neon } = require("@netlify/neon");
 
 function generateUserId() {
   // 4 char random alphanumeric e.g. "a3f9"
-  return Math.random().toString(36).substring(2, 6);
+  return Math.random().toString(36).substring(2, 8);
 }
 
 exports.handler = async (event) => {
