72969c50463db3dd2e832746e6b19fda78cc3a19
Joel Jo <108486336+thagreatjoel@users.noreply.github.com>
2026-03-23 08:32:11 +0530

    Create admins.js

---
+++ netlify/functions/admins.js
@@ -0,0 +1,10 @@
+// ─────────────────────────────────────────
+//  ADMINS — Hardware Playground
+//  user_id codes here to grant admin access
+// ─────────────────────────────────────────
+
+const ADMINS = [
+    
+];
+
+module.exports = { ADMINS };
